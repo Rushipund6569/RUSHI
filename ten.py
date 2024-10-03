0b55600779fffea57451dc48a0281a865c3bf837
RUSHIKESH PUND <156410583+Rushipund6569@users.noreply.github.com>
2024-10-03 13:16:14 +0530

    Update ten.py

--- ten.py
+++ ten.py
@@ -1,14 +1,14 @@
 def add_ten_numbers():
-    """Adds 10 numbers provided by the user."""
+ 
 
     sum = 0
-    for i in range(10):
+    for i in range(5):
         number = float(input(f"Enter number {i + 1}: "))
         sum += number
     return sum
 
-# Get the sum from the function
+
 result = add_ten_numbers()
 
-# Print the result
+
 print("The sum of the 10 numbers is:", result)
